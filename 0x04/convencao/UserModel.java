01c3abc52a7cf8d884ecb58698dc5b80ecd0b2bc
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-25 10:49:11 -0300

    2. Convenções de Nomenclatura

---
+++ 0x04/convencao/UserModel.java
@@ -0,0 +1,29 @@
+// classe users, representando nossa entidade
+
+class UsersModel {
+
+    int idUser;
+
+    String descricao;
+
+    final static int IDADEMINIMA = 18;
+
+    // função que coleta todos os users
+
+    public List<UserModel> fetchUsers() {
+            return new ArrayList<>();
+    }
+
+    // função que coleta todos os users
+
+    public List<UserModel> allUsers() {
+            return new ArrayList<>();
+    }
+
+    // função que coleta todos os users
+
+    public List<UserModel> getUsers() {
+            return new ArrayList<>();
+    }
+
+}
